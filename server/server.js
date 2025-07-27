00915b311b4cd508941abb72cfe29db4563e4fb7
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-27 17:07:47 -0300

    remove vercel.json

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ server.use('/cover', express.static(path.resolve(dirname, 'files', 'cover')));
 server.use('/game', express.static(path.resolve(dirname, 'files', 'game_file')));
 server.use(cors());
 server.use(router_login, router_new_hack, router_search, router_user_infs, router_hacks_infs);
-server.listen(process.env.PORT, process.env.HOST, () => {
+server.listen(process.env.PORT, process.env.HOST||"0.0.0.0", () => {
       console.log(`running on ${process.env.HOST}:${process.env.PORT}`);
 });
