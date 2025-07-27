2f0d2fbcf263209fa8479f81632428a3ade349d2
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-27 18:29:33 -0300

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 const DB = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    waitForConnections: true,
-    connectionLimit: 10
 });
 export default DB;
