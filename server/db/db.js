5af7fc9c170624bb4e6918757cc5a674070f5b54
ALAN-MS-SU <origin28baker@outlook.com>
2025-07-27 17:25:09 -0300

    update .env

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 import dotenv from 'dotenv';
 dotenv.config();
 const DB = new pg.Pool({
-    connectionString:process.env.uri,
+    connectionString: process.env.DATABASE_URL,
     waitForConnections: true,
     connectionLimit: 10
 });
